ad61509d3b1b5648a5de444fc0d09e9827b63d3b
fill1890 <andrew.walls2@gmail.com>
2016-09-29 19:24:23 +1000

    Initial Commit

---
+++ The Budgeter/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  The Budgeter
+//
+//  Created by Andrew Walls on 29/9/16.
+//  Copyright © 2016 Andrew Walls. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
